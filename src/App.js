ade3d59bd5136438eeed9fbf25bc90c40dbbcd70
costikluciu2020 <costikluciu2020@gmail.com>
2022-12-15 11:36:40 +0000

    Define the sesion area

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import {React} from "react";
+import React from 'react'
+
+
+
 
 function App() {
+const [displayTime, setDisplayTime] = React.useState(25*60);
+
+const formatTime = (time) => {
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    return (
+    (minutes < 10 ? "0" + minutes : minutes) + 
+    ":" + 
+    (seconds < 10 ? "0" + seconds : seconds)
+
+    );
+}
+
   return (
     <div className="App">
-    <h1>Pomodoro Timer</h1>
+    
+      <div>
+        <h1>{formatTime(displayTime)}</h1>
+      </div>
+
+
     </div>
   );
 }
