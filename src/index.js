0f7c2b411de64007da81480f97e86dd8caffb7dd
costikluciu2020 <costikluciu2020@gmail.com>
2022-12-16 11:40:28 +0000

    fixed the controlTime() to behave as it suppose to

--- src/index.js
+++ src/index.js
@@ -16,11 +16,24 @@ import "./style.css";
 
 
 function App() {
+    
 const [displayTime, setDisplayTime] = React.useState(25*60);
 const [breakTime, setBreakTime] = React.useState(5*60);
 const [sessionTime, setSessionTime] = React.useState(25*60);
 const [timerOn, setTimerOn] = React.useState(false);
 const [onBreak, setOnBreak] = React.useState(false);
+const [breakAudio, setBreakAudio] = React.useState(new Audio("static-noise.mp3"));
+
+
+/*
+// Testing the bleep 
+const [displayTime, setDisplayTime] = React.useState(6);
+const [breakTime, setBreakTime] = React.useState(2);
+const [sessionTime, setSessionTime] = React.useState(5);
+const [timerOn, setTimerOn] = React.useState(false);
+const [onBreak, setOnBreak] = React.useState(false);
+const [breakAudio, setBreakAudio] = React.useState(new Audio("static-noise.mp3"));
+*/
 
 
 
@@ -57,6 +70,114 @@ const changeTime = (amount, type)=>{
 
 }
 
+/*
+
+const controlTime = () => {
+    let second = 1000;
+    let date = new Date().getTime();
+    let nextDate = new Date().getTime() + second;
+    let onBreakVariable = onBreak;
+
+
+    console.log("onBreak=", onBreak);
+
+    if (!timerOn) {
+        let interval = setInterval(()=>{
+            date = new Date().getTime();
+            if (date > nextDate){
+                setDisplayTime((prev) => {
+                    if (prev <= 0  && !onBreakVariable){
+                        playBreakSound();
+                        onBreakVariable = true;
+                        setOnBreak(true);
+                        return breakTime;
+
+                    } else if (prev <= 0  && onBreakVariable){
+                        playBreakSound();
+                        onBreakVariable = false;
+                        setOnBreak(false);
+                        return sessionTime;
+
+                    }
+                    return prev - 1;
+                });
+                nextDate = nextDate + second;
+            }
+        }, 30);
+        localStorage.clear();        
+        localStorage.setItem("interval-id", interval);
+
+    }
+
+    if (timerOn){
+        clearInterval(localStorage.getItem("interval-id"));
+    }
+    setTimerOn(!timerOn);
+
+
+}
+*/
+
+
+
+const controlTime = () => {
+    let second = 1000;
+    let date = new Date().getTime();
+    let nextDate = new Date().getTime() + second;
+    let onBreakVariable = onBreak;
+    let displayTimeVariable = displayTime;
+
+
+    console.log("onBreak=", onBreak);
+    let countFireSetInterval = 0;
+    if (!timerOn) {
+        let interval = setInterval(()=>{
+
+            countFireSetInterval += 1;
+            console.log("countFireSetInterval=", countFireSetInterval);
+            
+            
+
+            displayTimeVariable = displayTimeVariable - 1;
+            // If the displayed time is negative and we are not on break 
+                    // Then switch the timer to the break intervak 
+            // If the display time is negative and we are on a break interval 
+                    // Then switch the timer to the session interval 
+            // Else just update the timmer by substracting 1 second (1000ms) from it 
+            if ((displayTimeVariable <= 0) && (onBreakVariable === false)){
+                playBreakSound();
+                onBreakVariable = true;
+                setOnBreak(true);
+                displayTimeVariable = breakTime;
+                setDisplayTime(breakTime);
+            } else if ((displayTimeVariable <= 0) && (onBreakVariable === true)){
+                playBreakSound();
+                onBreakVariable = false;
+                setOnBreak(false);
+                displayTimeVariable = sessionTime;
+                setDisplayTime(sessionTime);
+
+            } else {
+                setDisplayTime( displayTimeVariable);
+            }
+
+
+        }, 1000);
+
+        localStorage.clear();        
+        localStorage.setItem("interval-id", interval);
+
+    }
+
+    if (timerOn){
+        clearInterval(localStorage.getItem("interval-id"));
+    }
+    setTimerOn(!timerOn);
+
+
+}
+
+/*
 const controlTime = () => {
     let second = 1000;
     let date = new Date().getTime();
@@ -83,10 +204,21 @@ if (timerOn){
 setTimerOn(!timerOn);
 }
 
+*/
+
+
 const resetTime = () => {
     setDisplayTime(25*60);
     setBreakTime(5*60);
     setSessionTime(25*60);
+}
+
+
+
+const playBreakSound = () => {
+    breakAudio.currentTime = 0; 
+    breakAudio.play();
+
 }
 
 
@@ -113,6 +245,7 @@ return (
             />
         </div>
         <div>
+            <h3>{onBreak ? "Break":"Session" }</h3>
             <h1>{formatTime(displayTime)}</h1>
             <button 
                 className="btn btn-primary btn-lg"
